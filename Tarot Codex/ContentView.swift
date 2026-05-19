91b35fd79db5fecff42491af845be29e30de3d65
Will Parsons-Douglas <will@parsons-douglas.me>
2026-05-19 09:43:09 +0100

    Initial Commit

---
+++ Tarot Codex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tarot Codex
+//
+//  Created by William Parsons-Douglas on 19/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tarot Codex/Tarot_CodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tarot_CodexApp.swift
+//  Tarot Codex
+//
+//  Created by William Parsons-Douglas on 19/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tarot_CodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
